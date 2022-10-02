a67a3fe851894fdbecee22507834e7bccdb9eb7e
nicolas-source <nzheng8@student.ubc.ca>
2022-10-01 23:17:46 -0700

    cipher default example works

--- playfair_encryptor.py
+++ playfair_encryptor.py
@@ -1,5 +1,5 @@
 # cite: https://dev.to/karanmunjani/encryption-using-playfair-cipher-in-python-24l4
-
+import numpy as np
 
 key1 = "monarchybdefgiklpqstuvwxz"
 key2 = "playfirexmbcdghknoqstuvwz"
@@ -16,21 +16,50 @@ ctext3 = "RSSRDERSAEYRKIDP"
 ctext4 = "PDFLSMHGBSMDFSCFNZBXPDFGKATLTOMBSKBMTCKIMRVFLXIXSKLZSKNABEVFKM"
 ctext5 = "PDFLSMHGBSMDFSCFNZBXPDFGKATLTOMBSKBMTCKIMRVFLXIXSKLZSKNABEVFKM"
 
-ptext10 = "hide the gold in the tree stump"
+ptext10 = "hide the gold in the trexe stump"
 
 ctext10 = "BMODZ BXDNA BEKUD MUIXM MOUVI F"
 
 
-def playfairEncrypt(ptext):
-    ptext = ptext.replace(" ", "")
-    for i in range(0, len(ptext), 2):
-        print()
+def playfairEncryptBigram(ptext, keyMatrix):
+    np_keyMatrix = np.array(keyMatrix)
+    np_char1 = np.array(ptext[0])
+    np_char2 = np.array(ptext[1])
+
+    result1 = ""
+    result2 = ""
+
+    char1_loc = np.argwhere(np_keyMatrix == np_char1)[0]
+    char2_loc = np.argwhere(np_keyMatrix == np_char2)[0]
+
+    # if in same col
+    if char1_loc[1] == char2_loc[1]:
+        result1 = keyMatrix[(char1_loc[0] + 1) % 5][char1_loc[1]]
+        result2 = keyMatrix[(char2_loc[0] + 1) % 5][char1_loc[1]]
+
+    # if in same row
+    elif char1_loc[0] == char2_loc[0]:
+        result1 = keyMatrix[char1_loc[0]][(char1_loc[1] + 1) % 5]
+        result2 = keyMatrix[char1_loc[0]][(char2_loc[1] + 1) % 5]
+
+    else:
+        result1 = keyMatrix[char1_loc[0]][char2_loc[1]]
+        result2 = keyMatrix[char2_loc[0]][char1_loc[1]]
+
+    return result1 + result2
 
 
-# def playfairEncryptBigram(ptext):
+def playfairEncrypt(ptext, keyMatrix):
+    ptext = ptext.replace(" ", "").upper()
 
+    if len(ptext) % 2 == 1:
+        ptext += 'Z'
 
-# playfairEncrypt(ptext10)
+    result = ""
+    for i in range(0, len(ptext) - 1, 2):
+        result += playfairEncryptBigram(ptext[i] + ptext[i + 1], keyMatrix)
+
+    return result
 
 
 def generateKeyMatrix(key):
@@ -50,4 +79,10 @@ def generateKeyMatrix(key):
     return keyMatrix
 
 
-print(generateKeyMatrix(key2))
+keyMatrix = generateKeyMatrix(key2)
+
+print(playfairEncrypt(ptext10, keyMatrix))
+
+# print(playfairEncryptBigram("HI", keyMatrix))
+# playfairEncryptBigram("DE", keyMatrix)
+# playfairEncryptBigram("TH", keyMatrix)
