777418b7973ebd5aaf6f6f5e0ca229c712afa2f3
nicolas-source <nzheng8@student.ubc.ca>
2022-10-24 16:08:30 -0700

    A3 inclass completed

--- Assignment3/A3.py
+++ Assignment3/A3.py
@@ -1,28 +1,35 @@
 from hashlib import sha256
+import base64
+from datetime import datetime as dt
 import requests
 
 hash_of_preceding_coin = "a9c1ae3f4fc29d0be9113a42090a5ef9fdef93f5ec4777a008873972e60bb532"
-# id_of_miner = sha256("1")
 
 input_ = "1"
 id_of_miner = sha256(input_.encode('utf-8')).hexdigest()
-print("id_of_miner")
+
+
+print("public miner id")
 print(id_of_miner)
 
-coin_blob = "0000"  # we need to find this that satisfies the hash
-a = sha256(("CPEN 442 Coin" + "2022" + hash_of_preceding_coin + coin_blob + id_of_miner).encode('utf-8')).hexdigest()
+time_took = []
+start_time = dt.now()
+for i in range (0x0, 0x10000000):
+    coin_blob = str(i)
+    a = sha256(("CPEN 442 Coin" + "2022" + hash_of_preceding_coin + coin_blob + id_of_miner).encode('utf-8')).hexdigest()
+    if a.startswith("0000000"):
+        end_time = dt.now()
+        time_took.append((end_time - start_time).total_seconds() * 1000)
+        print("hash value: " + a)
+        print("coin blob: " + coin_blob)
+        b = base64.b64encode(bytes(coin_blob, 'utf-8'))
+        base64_str = b.decode('utf-8')
+        print("encoded blob: " + base64_str)
+        start_time = dt.now()
+        
+print("the average milisecond it takes to mine a coin is " + str(sum(time_took)/len(time_took)))
 
-print("a")
-print(a)
 
-# Need to verify with http://cpen442coin.ece.ubc.ca/verify_example_coin
-url = "http://cpen442coin.ece.ubc.ca/verify_example_coin"
 
-def verify_coin(coin_blob, id_of_miner):
-    data = {
-        "coin_blob": coin_blob,
-        "id_of_miner": id_of_miner
-    }
 
-    response = requests.post(url, json=data)
-    print("Verification result: " + response + "\n")
+        
