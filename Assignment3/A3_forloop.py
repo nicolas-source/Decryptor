037302d21b51a6e8269d5f461bc2b03b82f8e778
nicolas-source <nzheng8@student.ubc.ca>
2022-10-24 15:47:52 -0700

    changes

--- Assignment3/A3_forloop.py
+++ Assignment3/A3_forloop.py
@@ -12,12 +12,23 @@ id_of_miner = sha256(input_.encode('utf-8')).hexdigest()
 print("public miner id")
 print(id_of_miner)
 
+url = "http://cpen442coin.ece.ubc.ca/verify_example_coin"
+
+def verify_coin(coin_blob, id_of_miner):
+    data = {
+        "coin_blob": coin_blob,
+        "id_of_miner": id_of_miner
+    }
+
+    response = requests.post(url, json=data)
+    print("Verification result: " + response + "\n")
+
 time_took = []
+start_time = dt.datetime()
 for i in range (0x0, 0x100000):
-    start_time = dt.datetime()
     coin_blob = str(i)
     a = sha256(("CPEN 442 Coin" + "2022" + hash_of_preceding_coin + coin_blob + id_of_miner).encode('utf-8')).hexdigest()
-    if a.startswith("000000"):
+    if a.startswith("000000") and verify_coin(coin_blob, id_of_miner):
         end_time = dt.datetime()
         time_took.push((end_time - start_time).total_seconds())
         print("hash value: " + a)
@@ -29,14 +40,7 @@ for i in range (0x0, 0x100000):
         
 print("the average amount of time it takes to mine a coin is " + sum(time_took)/len(time_took))
 
-url = "http://cpen442coin.ece.ubc.ca/verify_example_coin"
 
-def verify_coin(coin_blob, id_of_miner):
-    data = {
-        "coin_blob": coin_blob,
-        "id_of_miner": id_of_miner
-    }
 
-    response = requests.post(url, json=data)
-    print("Verification result: " + response + "\n")
+
         
